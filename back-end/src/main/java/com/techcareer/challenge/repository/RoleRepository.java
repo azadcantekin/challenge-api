fb5e8e204bed4df0d9c996a2ad469d3cb8f2895d
Azad Can Tekin <77458116+azadcantekin@users.noreply.github.com>
2023-06-26 16:22:06 +0300

    Delete back-end directory

--- back-end/src/main/java/com/techcareer/challenge/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.techcareer.challenge.repository;
-
-import com.techcareer.challenge.data.model.RoleModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<RoleModel , Integer> {
-}
--- back-end/src/main/java/com/techcareer/challenge/service/CardService.java
+++
@@ -1,9 +0,0 @@
-package com.techcareer.challenge.service;
-
-import com.techcareer.challenge.data.dto.CardDto;
-
-public interface CardService {
-    int addCard(CardDto cardDto);
-    CardDto getCard(Integer cardId);
-    void deleteCard(Integer cardId);
-}
